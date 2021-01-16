2b9a6a7b5ee6a1efd9915e7fdff578eef6bb8fc1
Craig Main <craigmain@Craigs-MacBook-Pro.local>
2021-01-16 11:07:53 +0200

    initail testing

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,24 @@
 #include <Arduino.h>
+#include <Servo.h>
+
+Servo myservo;
+
+int pos = 0;
 
 void setup() {
-  // put your setup code here, to run once:
+  myservo.attach(9);
+  Serial.begin(115200);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  for(pos = 0; pos < 180; pos += 1) {
+    myservo.write(pos);
+    delay(15);
+  }
+
+  for(pos = 180; pos >=1; pos -= 1) {
+    myservo.write(pos);
+    delay(15);
+  }
+  Serial.println("Running");
 }

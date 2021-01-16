d9455c8c4ad7506fbdfc4274612f0ad5cd263350
Craig Main <craigmain@Craigs-MacBook-Pro.local>
2021-01-16 11:31:21 +0200

    more

--- src/main.cpp
+++ src/main.cpp
@@ -3,22 +3,34 @@
 
 Servo myservo;
 
-int pos = 0;
+//int pos = 0;
+int potpin = 1;
+int val;
 
 void setup() {
-  myservo.attach(9);
+  myservo.attach(10);
   Serial.begin(115200);
 }
 
 void loop() {
-  for(pos = 0; pos < 180; pos += 1) {
-    myservo.write(pos);
-    delay(15);
-  }
+  val = analogRead(potpin);
+  val = map(val, 0, 1025, 0, 179);
+  myservo.write(val);
+  Serial.println(val);
 
-  for(pos = 180; pos >=1; pos -= 1) {
-    myservo.write(pos);
-    delay(15);
-  }
-  Serial.println("Running");
+
+  // for(pos = 0; pos < 180; pos += 1) {
+  //   myservo.write(pos);
+  //   Serial.print("Setting pos to: ");
+  //   Serial.println(pos);
+  //   delay(15);
+  // }
+
+  // for(pos = 180; pos >=1; pos -= 1) {
+  //   myservo.write(pos);
+  //   Serial.print("Setting pos to: ");
+  //   Serial.println(pos);
+  //   delay(15);
+  // }
+  //Serial.println("Running");
 }
